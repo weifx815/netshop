249f8d0756082c70f0a00ada476caf0ac57613e8
weifx815 <393423373@qq.com>
2022-06-22 22:51:06 +0800

    注册页面验证码倒计时修改

--- static/js/user/register.js
+++ static/js/user/register.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function(){
     registerBinOnclick();
-    phoneCodeBinOnclick();
+    $("#getPhoneCode").click(getPhoneCode);
 });
 
 function registerBinOnclick(){
@@ -80,29 +80,7 @@ function ifRegister(code){
     return flag;
 }
 //获取手机验证码
-let phoneCodeBinOnclick = function(){
-    $("#getPhoneCode").click(function(){
-        let phone_number = $("#phone_number").val();
-        if(phone_number==""){
-            $("#phone_number_error").text("手机号不允许为空");
-            return false;
-        }
-        $.ajax({
-            type: 'get',
-            url: '/user/register/phonecode/',
-            data: {'code': phone_number},
-            async: true,
-            success: function(rdata) {
-                $("#phone_code").val(rdata);
-                countDown();
-                return false;
-            },
-            error:function (rdata){
-                $("#phone_number_error").text("系统出错了，请重新获取");
-            }
-        });
-    });
-}
+
 //验证邮箱是否正确
 function isEmail(email){
     let szReg=/^[a-z0-9A-Z]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\.)+[a-z]{2,}$/;
@@ -115,22 +93,43 @@ function isPhone(code){
     let bChk=szReg.test(code);
     return bChk;
 }
-//
+function getPhoneCode(){
+    let phone_number = $("#phone_number").val();
+    if(phone_number==""){
+        $("#phone_number_error").text("手机号不允许为空");
+        return false;
+    }
+        $.ajax({
+            type: 'get',
+            url: '/user/register/phonecode/',
+            data: {'code': phone_number},
+            success: function(rdata) {
+                $("#phone_code").val(rdata);
+                countDown();
+                return false;
+            },
+            error:function (rdata){
+                $("#phone_number_error").text("系统出错了，请重新获取");
+            }
+        });
+}
 //每1秒执行函式countDown()
 function countDown(){
-    $("#getPhoneCode").unbind();
+    $("#getPhoneCode").off('click',getPhoneCode);
     $("#getPhoneCode").prop("disabled",true);
+    $("#getPhoneCode").css("cursor","not-allowed");
     $("#phone_number_error").text("");
-    let times = 10;
-    let timer = setInterval(function(){
+    let times = 60;
+    let times_0 = setInterval(function (){
         times = times - 1;
         if(times>1){
-            $("#getPhoneCode").text(times+"秒后重新获取验证码");
+            $("#getPhoneCode").text(times+" 秒后重新获取验证码");
         }else{
-            clearInterval(times);
+            clearInterval(times_0);
+            $("#getPhoneCode").click(getPhoneCode);
             $("#getPhoneCode").prop("disabled",false);
-            $("#getPhoneCode").on('click', phoneCodeBinOnclick);
             $("#getPhoneCode").text("点击获取手机验证码");
+            $("#getPhoneCode").css("cursor","pointer");
         }
-    }, 1000);
+    },1000)
 }
