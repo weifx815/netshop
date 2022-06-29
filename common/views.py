367b10f8109236c8b3ce3cd17bdc8f6ccb2c97b2
weifx815 <393423373@qq.com>
2022-06-30 00:44:42 +0800

    优化获取菜单代码

--- common/views.py
+++ common/views.py
@@ -6,31 +6,35 @@ from common.forms import menuForm
 
 
 def userMenu(request):
-    menu_list = models.SysMenu.objects.all().order_by('menu_code')
-    return render(request, "sysMenu.html", {"menu_list": menu_list})
+    return render(request, "sysMenu.html", {"menu_list": get_Menu_List("ALL")})
 
 
 def userRose(request):
     role_list = models.SysRole.objects.all()
-    return render(request, "sysRole.html", {"role_list": role_list})
+    return render(request, "sysRole.html", locals())
 
 
 def menuEdit(request):
-    print("进来了=================================")
-    qmenulsit = models.SysMenu.objects.filter(menu_level='1').order_by('menu_code')
-    return render(request, "menuEdit.html", {"qmenulsit": qmenulsit})
+    get_Menu_List('1')
+    return render(request, "menuEdit.html", locals())
+
+
+def get_Menu_List(menu_level):
+    if menu_level == "ALL":
+        menu_list = models.SysMenu.objects.all().order_by('menu_code')
+    else:
+        menu_list = models.SysMenu.objects.filter(menu_level=menu_level, menu_status='Y').order_by('menu_code')
+    return menu_list
 
 
 def menuSave(request):
     if request.method == "GET":
         menu_form = menuForm.MenuModelForm()
-        qmenulsit = models.SysMenu.objects.filter(menu_level='1')
-        return render(request, "menuEdit.html", {"qmenulsit": qmenulsit, "menu_form": menu_form})
+        return render(request, "menuEdit.html", {"menu_form": menu_form, "menu_list": get_Menu_List('1')})
     if request.method == "POST":
         menu_form = menuForm.MenuModelForm(data=request.POST)
         if menu_form.is_valid():
             menu_form.save()
             return JsonResponse({'status': True, 'url': '/common/menu/'})
         else:
-            qmenulsit = models.SysMenu.objects.filter(menu_level='1')
-            return render(request, "menuEdit.html", {"qmenulsit": qmenulsit, "menu_form": menu_form})
+            return render(request, "menuEdit.html", {"menu_form": menu_form, "menu_list": get_Menu_List('1')})
